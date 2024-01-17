965d481c6e22b37bd7ec1f40564e24080c1b2847
Bojan Antolović <bojan.antolovic@gmail.com>
2024-01-17 17:47:27 +0100

    Rhetos.LightDMS demo.

--- src/Bookstore.Service/Startup.cs
+++ src/Bookstore.Service/Startup.cs
@@ -47,6 +47,7 @@ namespace Bookstore.Service
                 .AddHostLocalization()
                 .AddImpersonation()
                 .AddDashboard()
+                .AddLightDMS()
                 .AddRestApi(o =>
                 {
                     o.BaseRoute = "rest";
