af6e17bd8143b98a63cc423342c7784a26e3f7a1
Bojan Antolović <bojan.antolovic@gmail.com>
2024-09-19 17:36:13 +0200

    Example of a custom method that returns data in a custom format.

---
+++ src/Bookstore.Service/DslScripts/BookCount.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookstore.Repositories
+{
+    public partial class BookCount_Repository
+    {
+        public partial IEnumerable<BookCount> Load(BookCountFilter parameter)
+        {
+            var q = _domRepository.Bookstore.Book.Query();
+
+            if (parameter.Od != null)
+                q = q.Where(book => book.PublishDate >= parameter.Od);
+
+            if (parameter.Do != null)
+                q = q.Where(book => book.PublishDate <= parameter.Do);
+
+            int count = q.Count();
+
+            return new[]
+            {
+                new BookCount
+                {
+                    Count = count
+                }
+            };
+        }
+    }
+}
